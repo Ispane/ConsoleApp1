9e992ee94b74bae6f327cb2e862a0595523e7bf9
Admin <Admin@DESKTOP-9H2KH00>
2022-02-18 15:57:53 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Exam.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Exam : FinalExam
+    {
+        public float SumEst { get; set; }
+        public float Quantity { get; set; }
+
+        public override double GetEstimation() => SumEst / Quantity;
+    }
+}
